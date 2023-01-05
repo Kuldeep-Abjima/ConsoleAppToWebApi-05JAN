6d5ff77e3055cfaf66d28204d21fb16f476971aa
Kuldeep-Abjima <119651692+Kuldeep-Abjima@users.noreply.github.com>
2023-01-05 18:47:58 +0530

    Add project files.

---
+++ ConsoleAppToWebApi/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace ConsoleAppToWebApi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(WebHost =>
+            {
+                WebHost.UseStartup<StartUp>();
+            });
+        }
+    }
+}
---
+++ ConsoleAppToWebApi/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleAppToWebApi
+{
+    public class StartUp
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+        public void Configure(IApplicationBuilder applicationBuilder, IWebHostEnvironment env) 
+        {
+
+
+            applicationBuilder.UseRouting();
+        }
+    }
+}
